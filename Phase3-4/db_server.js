8b7104c5b9228cad853ee0e645fcfb34afab888e
JazzType <akash.kulhalli@gmail.com>
2017-05-15 16:48:55 +0530

    Added PUT, POST and updated GET actions

--- Phase3-4/db_server.js
+++ Phase3-4/db_server.js
@@ -1,50 +1,128 @@
 var express = require('express');
 var Datastore = require('nedb');
+var bodyParser = require('body-parser');
 var db = null;
 var app = express();
-app.get('/', function(req, res) {
-	for(name in req.query) {
-		console.log(name);
-	}
-	//res.write(req.query.tid + ' received\n');
-	db = new Datastore(req.query.tid);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.put('/', function(request, response) {
+	console.log("=====PUT=====");
+	db = new Datastore(request.query.tid);
 	db.loadDatabase();
-	db.find({}, function(error, docs) {
+	db.find({}, function (error, docs) {
 		if(docs.length == 0) {
 			console.log('Invalid tenant ID');
-			res.json({status: 404, message: 'Invalid tenant ID'});
+			response.json({status: 404, message: 'Invalid tenant ID\n'});			
 		}
-		else {
-			db.find({bid: req.query.bid}, function(err, docs) {
-				if(docs.length != 0) {
-					console.log("Retrieved:");
-					console.log(docs[0]);
-					res.end(docs[0].bid);
+		else {		
+			db.findOne({bid: String(request.query.bid)}, function(err, doc) {
+				if(doc != null) {
+					console.log('[ INF ] Match found for ' + request.query.bid);
+				
+					var insDoc = {};
+					
+					for(name in request.query) {					
+						insDoc[name] = request.query[name];
+					}
+					console.log(insDoc);
+					db.insert(insDoc,
+										function(err, newDoc) {
+											if(!err) {
+												response.end('{status: 200, message: "ok"}\n');
+												console.log('[ INF ] Updated db entry');
+											}
+											else {
+												console.log(newDoc);
+												console.log('[ ERR ] Could not update db\n');
+											}
+										}
+									 );
 				}
 				else {
-					db.insert(
-					          {
-					          	bid: req.query.bid,
-					          	tid: req.query.tid,
-					          	title: req.query.title
-					          },
-					          function(err, newDoc) {
-					          	if(!err) {
-					          		console.log(newDoc);
-					          		res.end('Created new DB for ' + req.query.tid + '\n');
-					          	}
-					          	else {
-					          		console.log('Could not add to db\n');
-					          	}
-					          }
-				           );
+					var insDoc = {};
+					
+					for(name in request.query) {					
+						insDoc[name] = request.query[name];
+					}
+					console.log(insDoc);
+					db.insert(insDoc,
+										function(err, newDoc) {
+											if(!err) {
+												response.end('{status: 200, message: "ok"}\n');
+												console.log('[ INF ] Added db entry');
+											}
+											else {
+												console.log(newDoc);
+												console.log('[ ERR ] Could not update db\n');
+											}
+										}
+									 );
 				}
 			});
 		}
 	});
+	console.log("Request queued");
+	response.end("\n");
 });
-/*app.post('/', function(request, response) {
-	console.log('Creating new database for tenant ' + request.query.tid);
-	db = new Datastore(request.query)
-});*/
+app.post('/', function(request, response) {	
+	console.log('Creating new database for tenant ' + request.body.tid);
+	db = new Datastore(request.body.tid);
+	db.loadDatabase();
+	db.findOne({created: true}, function(error, doc) {
+		if(!error) {
+			if(doc != null) {
+				console.log(doc);
+				if(doc.created) {
+					console.log('db for tenant already exists\n');
+					response.end('{status: 409, message: "Conflict"}\n');
+				}
+			}
+			else {
+				db.insert({created: true}, function(err, doc) {});
+				response.end('{status: 200, message: "ok"}\n');
+			}
+		}
+	});	
+});
+
+app.get('/', function(request, response) {
+	console.log("=====GET_ONE=====");
+	db = new Datastore(request.query.tid);
+	db.loadDatabase();
+	db.find({}, function (error, docs) {
+		if(docs.length == 0) {
+			console.log('Invalid tenant ID');
+			response.json({status: 404, message: 'Invalid tenant ID\n'});			
+		}
+		else {				
+			db.findOne({bid: String(request.query.bid)}, function(err, doc) {
+				if(doc != null) {
+					response.json(doc);
+					response.end('{status: 200, message: "ok"}\n');
+				}
+				else {
+					response.end('{status: 404, message: "Book ID not found"}\n');
+				}
+			});
+		}
+	});
+	console.log("Request queued");	
+});
+app.get('/all', function(request, response) {
+	console.log("=====GET_ALL=====");
+	db = new Datastore(request.query.tid);
+	db.loadDatabase();
+	db.find({}, function (error, docs) {
+		if(docs.length == 0) {
+			console.log('Invalid tenant ID');
+			response.end("{status: 404, message: 'Invalid tenant ID\n'}");
+		}
+		else {
+			response.json(docs);
+			response.end('{status: 200, message: "ok"}\n');
+		}
+	});
+	console.log("Request queued");	
+});
 app.listen(4700);
